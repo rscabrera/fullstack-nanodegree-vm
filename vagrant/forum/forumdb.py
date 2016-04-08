0691064f74fdd3f5f0c58547e8f180c849d291b7
rscabrera <rob@roberts-mbp.softlayer.local>
2016-04-07 21:10:13 -0500

    cleaning up

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -11,7 +11,7 @@ import time
 def GetAllPosts():
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute("SELECT time, content, FROM posts ORDER BY time DESC")
+    c.execute("SELECT time, content FROM posts ORDER BY time DESC")
     posts = ({'content': str(row[1]), 'time': str(row[0])} for row in c.fetchall())
     DB.close()
     return posts
@@ -20,6 +20,6 @@ def GetAllPosts():
 def AddPost(content):
     DB = psycopg2.connect("dbname=forum")
     c = DB.cursor()
-    c.execute("INSERT INTO posts (content) VALUES ('%s')" % content)
+    c.execute("INSERT INTO posts (content) VALUES (%s)", (content))
     DB.commit()
     DB.close()
